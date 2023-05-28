9373cedf3624b1f87d2ea5243d9fe82f824590dd
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-28 12:42:29 +0200

    nix: pin on node 18

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
-      pkgs.nodejs
+      pkgs.nodejs_18
       pkgs.yarn
     ];
 }
